7bd379a6823ff2d407f595d61ea6e455e6d5dc96
Elgusto25 <56129309+Elgusto25@users.noreply.github.com>
2019-10-03 19:44:23 +0200

    Create isaac.js

---
+++ isaac.js
@@ -0,0 +1,15 @@
+(function () {
+
+
+    document.querySelectorAll('.card').forEach(card => {
+        card.addEventListener('click', function () {
+            card.classList.toggle('is-flipped');
+        });
+
+
+    })
+
+    
+})();
+
+
